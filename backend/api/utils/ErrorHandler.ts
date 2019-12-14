fe0cb03e9ec4b64b5b31c024f344849f0545b8a5
Valerii <Ihnatiev@users.noreply.github.com>
2019-12-14 17:26:20 +0200

    Update ErrorHandler.ts

--- backend/api/utils/ErrorHandler.ts
+++ backend/api/utils/ErrorHandler.ts
@@ -1,14 +1,14 @@
-import { Response, NextFunction } from "express";
-import { HTTPSClientError, HTTPS404Error } from "./httpsErrors";
+import { Response, NextFunction } from 'express';
+import { HTTPSClientError, HTTPS404Error } from './httpsErrors';
 
 export const notFoundError = () => {
-  throw new HTTPS404Error("Method not found.");
+  throw new HTTPS404Error('Method not found.');
 };
 
 export const clientError = (err: Error, res: Response, next: NextFunction) => {
   if (err instanceof HTTPSClientError) {
     console.warn(err);
-    res.status(err.statusCode).send(err.message);
+    res.status(err.statusCode).json({ message: err.message });
   } else {
     next(err);
   }
@@ -16,10 +16,10 @@ export const clientError = (err: Error, res: Response, next: NextFunction) => {
 
 export const serverError = (err: Error, res: Response, next: NextFunction) => {
   console.error(err);
-  if (process.env.NODE_ENV === "production") {
-    res.status(500).send("Internal Server Error");
+  if (process.env.NODE_ENV === 'production') {
+    res.status(500).json({ message: 'Internal Server Error' });
   } else {
-    res.status(500).send(err.stack);
+    res.status(500).json({ message: err.stack });
   }
 };
 
