76aa8888763f3e2ec3485f954246138d145484bd
Valerii <Ihnatiev@users.noreply.github.com>
2019-12-14 17:26:17 +0200

    Update routes.ts

--- backend/api/routes/routes.ts
+++ backend/api/routes/routes.ts
@@ -12,24 +12,24 @@ const userController = new UserController(mysqlConnection);
 
 export default [
   {
-    path: "/api/auth/signup",
-    method: "post",
+    path: '/api/auth/signup',
+    method: 'post',
     handler: [validateBody('new-user'), checkUserCreate,
     (req: Request, res: Response) => {
       userController.createUser(req, res);
     }]
   },
   {
-    path: "/api/auth/login",
-    method: "post",
+    path: '/api/auth/login',
+    method: 'post',
     handler: [validateBody('user-login'), checkUserLogin,
     async (req: Request, res: Response) => {
       await userController.loginUser(req, res);
     }]
   },
   {
-    path: "/api/employees",
-    method: "get",
+    path: '/api/employees',
+    method: 'get',
     handler: [validEmpList('emp-all'),
     async (req: Request, res: Response) => {
       await employeesController.findAllEmployees(req, res);
@@ -37,36 +37,36 @@ export default [
     }]
   },
   {
-    path: "/api/employees/:empID",
-    method: "get",
+    path: '/api/employees/:empID',
+    method: 'get',
     handler: [validEmpId('emp-id'),
     async (req: Request, res: Response) => {
       await employeesController.findEmployee(req, res);
     }]
   },
   {
-    path: "/api/employees/",
-    method: "post",
+    path: '/api/employees/',
+    method: 'post',
     handler: [validateBody('emp-create'), checkJwt,
     async (req: Request, res: Response) => {
       await employeesController.createEmployee(req, res);
     }]
   },
   {
-    path: "/api/employees/:empID",
-    method: "put",
+    path: '/api/employees/:empID',
+    method: 'put',
     handler: [validateBody('emp-create'), checkJwt,
     async (req: Request, res: Response) => {
       await employeesController.updateEmployee(req, res);
     }]
   },
   {
-    path: "/api/employees/:empID",
-    method: "delete",
+    path: '/api/employees/:empID',
+    method: 'delete',
     handler: [validEmpId('emp-id'), checkJwt,
     async (req: Request, res: Response) => {
       await employeesController.deleteEmployee(req, res);
     }]
   }
-]
+];
 
