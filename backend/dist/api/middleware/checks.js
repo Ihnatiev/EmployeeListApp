c955ffb9e7a5b798000f4fc32d50e4ae3f43e623
Valerii <Ihnatiev@users.noreply.github.com>
2019-12-14 17:26:26 +0200

    Update checks.js

--- backend/dist/api/middleware/checks.js
+++ backend/dist/api/middleware/checks.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const httpsErrors_1 = require("../utils/httpsErrors");
 exports.checkUserCreate = (req, res, next) => {
     if (!req.body.name || !req.body.email || !req.body.password) {
-        throw new httpsErrors_1.HTTPS400Error("All fields must be filled");
+        throw new httpsErrors_1.HTTPS400Error('All fields must be filled');
     }
     else {
         next();
@@ -11,7 +11,7 @@ exports.checkUserCreate = (req, res, next) => {
 };
 exports.checkUserLogin = (req, res, next) => {
     if (!req.body.email || !req.body.password) {
-        throw new httpsErrors_1.HTTPS400Error("All fields must be filled");
+        throw new httpsErrors_1.HTTPS400Error('All fields must be filled');
     }
     else {
         next();
