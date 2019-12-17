91482693d1f74dbfcb74fc7b76019007d6d58cdb
Valerii <Ihnatiev@users.noreply.github.com>
2019-12-17 18:10:55 +0200

    deleted dist

--- backend/dist/api/config/secret.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    jwtSecret: "this_secret_should_be_longer",
-    payload: {
-        email: 'qwerty123456@gmail.com',
-        password: 'qwerty123456'
-    }
-};
