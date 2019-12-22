70aff7fcf4a58bcf9724e1d603164c5943ee89a1
Valerii <Ihnatiev@users.noreply.github.com>
2019-12-22 14:46:39 +0200

    Create cors.ts

---
+++ backend/api/helpers/cors.ts
@@ -0,0 +1,14 @@
+export function init(app: any) {
+  app.use((req: any, res: any, next: any) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, X-Auth-Token, Authorization'
+    );
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+    );
+    next();
+  });
+}
