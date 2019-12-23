e9da69b05e74b00e12a794db5c016d2294167884
Valerii <Ihnatiev@users.noreply.github.com>
2019-12-23 19:29:00 +0200

    Update userController.ts

--- backend/api/controllers/userController.ts
+++ backend/api/controllers/userController.ts
@@ -16,19 +16,19 @@ export class UserController {
       const { name, email, password } = req.body;
       await this.userService.signup(name, email, password)
         .then(user => {
-          res.status(201).json({
+          return res.status(201).json({
             success: true,
             message: 'User created!',
             userId: user.id
           });
         }).catch(err => {
-          res.status(500).json({
+          return res.status(500).json({
             success: false,
             message: 'Sorry. That email already exists. Try again.'
           });
         });
     } catch {
-      res.status(500).json({
+      return res.status(500).json({
         success: false,
         message: 'Invalid authentication credentials!'
       });
@@ -45,25 +45,29 @@ export class UserController {
           success: false,
           message: 'Auth failed! Check your email.'
         });
-      } else {
+      }
+      if (user) {
         const result = await bcrypt.compare(password, user.password);
-        const token: string = jwt.sign(
-          { userId: user.id, email: user.email },
-          secret.jwtSecret, { algorithm: 'HS256', expiresIn: '1h' });
-        (!result) ?
-          res.status(401).json({
+        if (!result) {
+          return res.status(401).json({
             success: false,
             message: 'Email and password does not match.'
-          }) :
-          res.status(200).json({
-            token: token,
+          });
+        }
+        if (result) {
+          const token: string = jwt.sign(
+            { userId: user.id, email: user.email },
+            secret.jwtSecret, { algorithm: 'HS256', expiresIn: '1h' });
+          return res.status(200).json({
+            token,
             expiresIn: 3600,
             userId: user.id,
             userName: user.name
           });
+        }
       }
-    } catch (err) {
-      res.status(500).json({
+    } catch (error) {
+      return res.status(500).json({
         success: false,
         message: 'Server error'
       });
