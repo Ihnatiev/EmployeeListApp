f64691fc4876c1be91553d06e5dcb3b4223a1309
vvalerii <valerii.ign@gmail.com>
2020-01-11 03:02:21 +0200

    Update app.ts

--- backend/api/app.ts
+++ backend/api/app.ts
@@ -1,10 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 import swaggerUi from 'swagger-ui-express';
 import swagDoc from './swagger/swagger.json';
 
 const app = express();
 require('./helpers/cors').init(app);
+
+mongoose
+    .connect(
+        "mongodb+srv://valerii_mongo:valerii_mongo@cluster0-mypsi.mongodb.net/test",
+        { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connected to Mongo database!");
+    })
+    .catch(() => {
+        console.log("Connection failed!");
+    });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
